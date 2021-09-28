82b811efedf456a40cce23f24d2de3594bab1916
aucker <aucker@qq.com>
2021-09-28 11:03:21 +0800

    api entry

--- magazine-cutout/src/lib.rs
+++ magazine-cutout/src/lib.rs
@@ -6,10 +6,32 @@ use std::collections::HashMap;
 
 pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
 
-    magazine
+    /*magazine
         .iter()
-        .fold(0, |acc, &word| acc + note.contains(&word) as usize) >= note.len()
+        .fold(0, |acc, &word| acc + note.contains(&word) as usize) >= note.len()*/
 
+    let mut mag: HashMap<&str, u32> = HashMap::new();
 
+    magazine
+        .iter().for_each(|word| {
+        if let Some(val) = mag.get_mut(word) {
+            *val += 1
+        } else {
+            mag.insert(word, 1)
+        }
+    });
+
+    for word in note.iter() {
+        if let Some(val) = mag.get_mut(word) {
+            if * val == 1 {
+                mag.remove(word)
+            } else {
+                *val -= 1
+            }
+        } else {
+            return false
+        }
+    }
+    true
     //unimplemented!()
 }
